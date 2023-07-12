1e3d4807478ee9649edc56fd0382dfbe32b1d978
nav-28 <pwadhwa@ualberta.ca>
2023-07-12 15:53:26 -0600

    Add tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Grid } from "./grid";
+import { tutorialContent } from './tutorial';
 
 var grid = new Grid(window.innerHeight / (25 * 2), window.innerWidth / 25);
 
@@ -47,11 +48,39 @@ pathButton.addEventListener('click', () => {
   if (!grid.animationRunning) {
     grid.clearPath();
   }
-})
+});
 
-//window.addEventListener("resize", updateGridSize);
+// tutorial
+const tutSkip = document.getElementById('tutorial-skip') as HTMLButtonElement;
+tutSkip.addEventListener('click', () => {
+  const tutorialDiv = document.getElementById('tutorial');
+  if (!tutorialDiv) return;
+  tutorialDiv.style.display = "none";
+});
 
+const tutorialContentDiv = document.getElementById('tutorial-content') as HTMLDivElement;
+const tutCounterDiv = document.getElementById('tut-count') as HTMLDivElement;
+var tutorialCounter = 0;
+tutorialContentDiv.innerHTML = tutorialContent[tutorialCounter];
+tutCounterDiv.innerHTML = `${tutorialCounter + 1}/${tutorialContent.length}`;
 
+const tutNextButton = document.getElementById('tutorial-next') as HTMLButtonElement;
+tutNextButton.addEventListener('click', () => {
+  if (tutorialCounter < tutorialContent.length - 1) {
+    tutorialCounter = tutorialCounter + 1;
+    tutorialContentDiv.innerHTML = tutorialContent[tutorialCounter];
+    tutCounterDiv.innerHTML = `${tutorialCounter + 1}/${tutorialContent.length}`;
+  }
+});
+
+const tutPrevButton = document.getElementById('tutorial-prev') as HTMLButtonElement;
+tutPrevButton.addEventListener('click', () => {
+  if (tutorialCounter == 0) return;
+  tutorialCounter = tutorialCounter - 1;
+  tutorialContentDiv.innerHTML = tutorialContent[tutorialCounter];
+});
+
+// window.addEventListener("resize", updateGridSize);
 // TODO:
 // Add basic buttons: clear board, clear path, clear walls
 // Add Tutorial.
---
+++ src/tutorial.ts
@@ -0,0 +1,17 @@
+
+
+export const tutorialContent = [
+    `
+<h1>Welcome to Pathfinding Visualizer</h1>
+            <h3>This short tutorial will walk you through all of the features of this application.</h2>
+                <p>If you want to dive right in, feel free to press the "Skip Tutorial" button below. Otherwise, press
+                    "Next"!
+                </p>
+
+`,
+    `
+<h1>What is a Pathfinding Algorithm?</h1>
+<h3>At its core, a pathfinding algorithm seeks to find the shortest path between two points. This application visualizes various pathfinding algorithms in action, and more!</h3>
+<p>All of the algorithms on this application are adapted for a 2D grid, where 90 degree turns have a "cost" of 1 and movements from a node to another have a "cost" of 1.</p>
+`
+];
